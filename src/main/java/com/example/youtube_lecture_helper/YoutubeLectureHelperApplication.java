704bf0dd32a608f17c5b960ad45b36b9b86c29b1
noir-01 <blanc@DESKTOP-23KS9M5>
2025-04-08 14:38:31 +0900

    Build: Spring Init

---
+++ src/main/java/com/example/youtube_lecture_helper/YoutubeLectureHelperApplication.java
@@ -0,0 +1,13 @@
+package com.example.youtube_lecture_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeLectureHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeLectureHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/youtube_lecture_helper/YoutubeLectureHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.youtube_lecture_helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeLectureHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
