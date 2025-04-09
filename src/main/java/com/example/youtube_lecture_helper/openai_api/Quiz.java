f876a052501f237eb7f351afe16c41caf7c6737b
noir-01 <smu08054@gmail.com>
2025-04-10 01:41:02 +0900

    Fix: Quiz에 video Id 필드 추가

--- src/main/java/com/example/youtube_lecture_helper/openai_api/Quiz.java
+++ src/main/java/com/example/youtube_lecture_helper/openai_api/Quiz.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.example.youtube_lecture_helper.openai_api;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -6,13 +6,15 @@ import java.util.List;
 
 @Getter
 public class Quiz {
+    private String videoId;
     private String question;
     private List<String> options;
     private String correctAnswer;
     private int timestamp;
     private String comment; //설명
 
-    public Quiz(String question, List<String> options, String correctAnswer, String comment, int timestamp) {
+    public Quiz(String videoId, String question, List<String> options, String correctAnswer, String comment, int timestamp) {
+        this.videoId = videoId;
         this.question = question;
         this.options = options;
         this.correctAnswer = correctAnswer;
@@ -20,7 +22,8 @@ public class Quiz {
         this.comment = comment;
     }
     //주관식 문제: 기존 정답을 기반으로 채점 시 gpt에 요청하기
-    public Quiz(String question, String correctAnswer, int timestamp) {
+    public Quiz(String videoId, String question, String correctAnswer, int timestamp) {
+        this.videoId = videoId;
         this.question = question;
         this.correctAnswer = correctAnswer;
         this.timestamp = timestamp;
