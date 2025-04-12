5fea4822c0e291312d903bb589374d1e7a1ad871
noir-01 <smu08054@gmail.com>
2025-04-12 16:56:14 +0900

    Fix: CORS 모든 호스트 허용
    배포 시 수정 필요

--- src/main/java/com/example/youtube_lecture_helper/config/SecurityConfig.java
+++ src/main/java/com/example/youtube_lecture_helper/config/SecurityConfig.java
@@ -34,10 +34,11 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         // 허용할 Origin 설정
-        configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:3000",
-                "https://example.com"
-        ));
+        // configuration.setAllowedOrigins(Arrays.asList(
+        //         "http://localhost:3000",
+        //         "https://example.com"
+        // ));
+        configuration.setAllowedOrigins(Arrays.asList("*"));
 
         // 허용할 HTTP 메서드 설정
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
