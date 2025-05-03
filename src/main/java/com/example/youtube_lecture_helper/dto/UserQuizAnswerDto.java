09773f78cdb459a1f1d224c63b7def962a01a295
noir-01 <smu08054@gmail.com>
2025-05-03 18:08:32 +0900

    Fix: DTO 필요 없는 속성 제거

--- src/main/java/com/example/youtube_lecture_helper/dto/UserQuizAnswerDto.java
+++ src/main/java/com/example/youtube_lecture_helper/dto/UserQuizAnswerDto.java
@@ -8,9 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Getter @Setter @AllArgsConstructor @NoArgsConstructor
 public class UserQuizAnswerDto {
-    private long userId;
     private long quizId;
-    private boolean isShortQuiz;
     private String userAnswer;
     public long getQuizId(){
         return quizId;
