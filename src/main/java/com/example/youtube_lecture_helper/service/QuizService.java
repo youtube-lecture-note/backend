ab986ce6a075301567ff092c469ed3c10b5f0ff6
noir-01 <smu08054@gmail.com>
2025-05-03 18:08:19 +0900

    Fix: QuizLog 제거(QuizAttempt 및 QuizSet 이용)

--- src/main/java/com/example/youtube_lecture_helper/service/QuizService.java
+++ src/main/java/com/example/youtube_lecture_helper/service/QuizService.java
@@ -31,7 +31,6 @@ public class QuizService {
     private final UserRepository userRepository;
 
     private final OpenAIGptClient gptClient;    //주관식 정답 맞출 때 필요
-    private final QuizLogService quizLogService;
 
 //    public QuizService(OpenAIGptClient gptClient, QuizRepository quizRepository, QuizLogService quizLogService){
 //        this.gptClient = gptClient;
@@ -44,35 +43,35 @@ public class QuizService {
     }
 
     //틀린 결과만 제공
-    public List<Long> getWrongAnswerQuizIds (List<UserQuizAnswerDto> userQuizAnswerDtoList){
-        return userQuizAnswerDtoList.stream()
-                .filter(userQuizAnswerDto -> !isCorrect(userQuizAnswerDto)) //false만 필터링
-                .map(UserQuizAnswerDto::getQuizId)
-                .toList();
-    }
+    // public List<Long> getWrongAnswerQuizIds (List<UserQuizAnswerDto> userQuizAnswerDtoList){
+    //     return userQuizAnswerDtoList.stream()
+    //             .filter(userQuizAnswerDto -> !isCorrect(userQuizAnswerDto)) //false만 필터링
+    //             .map(UserQuizAnswerDto::getQuizId)
+    //             .toList();
+    // }
 
-    private boolean isCorrect(UserQuizAnswerDto userQuizAnswerDto){
-        boolean result;
-        long quizId = userQuizAnswerDto.getQuizId();
-        Quiz quiz = quizRepository.findById(quizId)
-                .orElseThrow(() -> new QuizNotFoundException(Long.toString(quizId)));
+    // private boolean isCorrect(UserQuizAnswerDto userQuizAnswerDto){
+    //     boolean result;
+    //     long quizId = userQuizAnswerDto.getQuizId();
+    //     Quiz quiz = quizRepository.findById(quizId)
+    //             .orElseThrow(() -> new QuizNotFoundException(Long.toString(quizId)));
 
-        if(quiz.isSelective()){  //객관식이면 단순비교
-            result = quiz.getCorrectAnswer().equals(userQuizAnswerDto.getUserAnswer());
-        }else{ //주관식이면 gpt한테 물어보기
-            result = gptClient.isCorrectSubjectiveAnswer(
-                    quiz.getQuestion(),
-                    quiz.getCorrectAnswer(),
-                    userQuizAnswerDto.getUserAnswer()
-            );
-        }
-        //오답일 경우 quizLogRepo에 오답 기록 저장
-        if(!result){
-            quizLogService.saveIncorrectAnswer(userQuizAnswerDto);
-        }
+    //     if(quiz.isSelective()){  //객관식이면 단순비교
+    //         result = quiz.getCorrectAnswer().equals(userQuizAnswerDto.getUserAnswer());
+    //     }else{ //주관식이면 gpt한테 물어보기
+    //         result = gptClient.isCorrectSubjectiveAnswer(
+    //                 quiz.getQuestion(),
+    //                 quiz.getCorrectAnswer(),
+    //                 userQuizAnswerDto.getUserAnswer()
+    //         );
+    //     }
+    //     //오답일 경우 quizLogRepo에 오답 기록 저장
+    //     if(!result){
+    //         quizLogService.saveIncorrectAnswer(userQuizAnswerDto);
+    //     }
 
-        return result;
-    }
+    //     return result;
+    // }
 
     @Transactional // Ensure all DB operations succeed or fail together
     public CreatedQuizSetDTO createQuizSetForUser(Long userId, int difficulty, String youtubeId, int numberOfQuestions) {
